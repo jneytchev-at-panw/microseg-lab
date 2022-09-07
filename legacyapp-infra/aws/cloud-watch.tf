394938a70750e901223e474ab25ec715aca4ddf7
Julian Neytchev <105320482+jneytchev-at-panw@users.noreply.github.com>
2022-09-06 21:56:44 -0500

    Update legacyapp-infra/aws/cloud-watch.tf
    allow ignoring of public acls in bucket1
    
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- legacyapp-infra/aws/cloud-watch.tf
+++ legacyapp-infra/aws/cloud-watch.tf
@@ -33,6 +33,7 @@ resource "aws_s3_bucket_public_access_block" "bpab1" {
   restrict_public_buckets = true
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls = true
 }
 
 resource "aws_kms_key" "kms1" {
