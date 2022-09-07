9fcb196f0e35578b868c9002b097378d28553dce
Julian Neytchev <105320482+jneytchev-at-panw@users.noreply.github.com>
2022-09-06 21:48:33 -0500

    Fixing instances and networking issues with BC, part1

---
+++ legacyapp-infra/aws/cloud-watch.tf
@@ -0,0 +1,41 @@
+resource "aws_flow_log" "flowlog1" {
+  log_destination      = aws_s3_bucket.bucket1.arn
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.vpc1.id
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "${var.prefix}-bucket1"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bssec1" {
+  bucket = aws_s3_bucket.bucket1.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.kms1.arn
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bver1" {
+  bucket = aws_s3_bucket.bucket1.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bpab1" {
+  bucket = aws_s3_bucket.bucket1.id
+  restrict_public_buckets = true
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+resource "aws_kms_key" "kms1" {
+  description             = "KMS key 1"
+  enable_key_rotation    = true
+}
